798f5a19955bc10cce12e89310f8fc08e32267cf
nivinmuhamed <muni24by@student.ju.se>
2025-02-21 12:03:57 +0100

    make it interactive
    i made the main picture on the homepage bigger when you click on it and then put in a reset button for the picture.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//help from classmate Ida Segolsson// 
+
+const selfie = document.getElementById("selfie");
+const reset = document.getElementById("reset");
+
+selfie.addEventListener("click", function (event) {
+  selfie.style.width = "1000px";
+});
+
+reset.addEventListener("click", function () {
+  selfie.style.width = "10%";
+});
